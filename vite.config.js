a86d55a7afa792772a5fddd819578c69abe74f77
salah <salahukutipuravan@gmail.com>
2021-12-29 18:03:01 +0530

    base things

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/deploying-article=preview-component/',
   plugins: [vue()],
   server:{
     watch:{
