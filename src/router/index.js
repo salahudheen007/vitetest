f3cc8ebc89ebdc64fd1840f2875fd3cf663e10d6
salah <salahukutipuravan@gmail.com>
2022-01-01 12:57:53 +0530

    vczv

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,10 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import CardBody from '/src/components/CardBody.vue'
-const routes=[ {
-    path: '/',
-    redirect: { name: 'CardBody' }
-  },
-  {
-    path: '/vitesite/h',
-    name: 'CardBody',
-    component: CardBody
-  }]
+const routes=[{
+    path:'/vitetest/ha',
+    name:'CardBody',
+    component:CardBody
+}]
 const router=createRouter({
     history:createWebHistory(),
     routes,
